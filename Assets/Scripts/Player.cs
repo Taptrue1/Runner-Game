cd7f68525379b23e2192fee3f5b32feaa8a8504f
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2021-12-11 19:37:54 +0300

    Реализовал прыжок

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,61 @@
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed;
     [SerializeField] private float _speedIncrease;
+    [SerializeField] private float _jumpForce;
+    [SerializeField] private float _gravity;
 
-    private Rigidbody _rigidbody;
-    private float _runStartTime;
+    private CharacterController _controller;
+    private Vector3 _moveDirection;
+    private float _startRunTime;
+    private Vector2 _startTouchPosition;
+    private Vector2 _endTouchPosition;
 
     private void Start()
     {
-        _rigidbody = GetComponent<Rigidbody>();
-        _runStartTime = Time.time;
+        _controller = GetComponent<CharacterController>();
+        _startRunTime = Time.time;
+    }
+    private void Update()
+    {
+        var input = GetSwipe();
+        var canJump = input == Vector2.up && _controller.isGrounded;
+
+        if (canJump)
+        {
+            _moveDirection.y = _jumpForce;
+        }
     }
     private void FixedUpdate()
     {
-        var speed = _speed + _speedIncrease * (Time.time - _runStartTime);
-        _rigidbody.velocity = new Vector3(0, 0, speed);
+        var speed = _speed + _speedIncrease * (Time.time - _startRunTime);
+        _moveDirection.z = _speed;
+        _moveDirection.y -= _gravity * Time.fixedDeltaTime;
+        _controller.Move(_moveDirection * Time.fixedDeltaTime);
+    }
+
+    private Vector2 GetSwipe()
+    {
+        var swipe = Vector2.zero;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            _startTouchPosition = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            _endTouchPosition = Input.mousePosition;
+            var input = _endTouchPosition - _startTouchPosition;
+
+            if (Mathf.Abs(input.x) > Mathf.Abs(input.y))
+                swipe = input.x < 0 ? Vector2.left : Vector2.right;
+            else
+                swipe = input.y < 0 ? Vector2.down : Vector2.up;
+        }
+
+        return swipe;
     }
 }
