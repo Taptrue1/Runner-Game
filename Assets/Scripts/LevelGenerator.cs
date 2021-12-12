b98a2866a0096244807baeba7f5aed859d3426a5
Taptrue <82027635+Taptrue1@users.noreply.github.com>
2021-12-12 21:17:33 +0300

    Реализовал бесконечно генерируемую карту

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour
+{
+    [Header("Level Position")]
+    [SerializeField] private Vector3 _levelPosition;
+    [Header("Level Parts Options")]
+    [SerializeField] private GameObject[] _levelParts;
+    [SerializeField] private float _partsLength;
+    [SerializeField] private int _partsCount;
+    [Header("Player Transform")]
+    [SerializeField] private Transform _player;
+
+    private List<GameObject> _partsPool;
+    private int _nextPartIndex;
+
+    private void Start()
+    {
+        _partsPool = new List<GameObject>();
+
+        for(int i = 0; i < _partsCount; i++)
+        {
+            SpawnPart();
+        }
+    }
+    private void Update()
+    {
+        CheckPartPass();
+    }
+
+    private void CheckPartPass()
+    {
+        var part = _partsPool[0];
+        var isPartPassed = part.transform.position.z + _partsLength / 2 < _player.position.z;
+
+        if (isPartPassed)
+        {
+            _partsPool.Remove(part);
+            Destroy(part);
+            SpawnPart();
+        }
+    }
+    private void SpawnPart()
+    {
+        var part = Instantiate(_levelParts[_nextPartIndex], _levelPosition, Quaternion.identity, transform);
+
+        _partsPool.Add(part);
+        _levelPosition.z += _partsLength;
+        _nextPartIndex = _nextPartIndex + 1 > _levelParts.Length - 1 ? 0 : _nextPartIndex + 1;
+    }
+}
